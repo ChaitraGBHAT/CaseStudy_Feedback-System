1c9beed6ca950dde0c8345aab0cc27f6c12bfc31
ChaitraGBHAT <94519246+ChaitraGBHAT@users.noreply.github.com>
2022-04-29 14:17:01 +0530

    Add files via upload

---
+++ Microservice/Feedbacks/Employee.Model/Employee/GetEmployeeByIdResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee.Model.Employee
+{
+    public class GetEmployeeByIdResponse
+    {
+        public Employee employee { get; set; }
+    }
+}
---
+++ Microservice/Feedbacks/Employee.Model/Employee/PostEmployeeResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Employee.Model.Employee
+{
+    public class PostEmployeeResponse
+    {
+        public int Id { get; set; }
+        public string message { get; set; }
+    }
+}
