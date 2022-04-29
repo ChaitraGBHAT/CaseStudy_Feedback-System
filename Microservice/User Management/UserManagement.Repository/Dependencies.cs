c0d705ca5270ae640f3f0d98ea06f92dc3c8c6de
ChaitraGBHAT <94519246+ChaitraGBHAT@users.noreply.github.com>
2022-04-29 13:54:52 +0530

    Add files via upload

---
+++ Microservice/User Management/UserManagement.Repository/Dependencies.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UserManagement.Repository.Interface;
+
+namespace UserManagement.Repository
+{
+    public static class Depenencies
+    {
+        public static IServiceCollection ConnectRepository(this IServiceCollection service)
+        {
+            service.AddTransient<IUserRepository, UserRepository>();
+            return service;
+        }
+    }
+}
---
+++ Microservice/User Management/UserManagement.Repository/Interface/IUserRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Models.Login;
+using UserManagement.Models.Register;
+using UserManagement.Models.UpdatePassword;
+
+namespace UserManagement.Repository.Interface
+{
+    public interface IUserRepository
+    {
+        Task<UserDetails> Login(Login users);
+
+        Task<UpdatePasswordResponse> UpdatePasswordAync(UpdatePassword updatePassword);
+
+       Task<RegisterResponse> Register(string email, string password, string roleName);
+    }
+}
+
