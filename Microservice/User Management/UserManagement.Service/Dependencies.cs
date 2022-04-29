41b5423871d2d92d112fa56a5a906cd1255c76fd
ChaitraGBHAT <94519246+ChaitraGBHAT@users.noreply.github.com>
2022-04-29 13:55:29 +0530

    Add files via upload

---
+++ Microservice/User Management/UserManagement.Service/Dependencies.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UserManagement.Repository;
+using UserManagement.Service.Interface;
+
+namespace UserManagement.Service
+{
+    public static class Depenencies
+    {
+        public static IServiceCollection ConnectService(this IServiceCollection service)
+        {
+            service
+                .ConnectRepository()
+                .AddTransient<IUserService, UserService>();
+            return service;
+        }
+    }
+}
---
+++ Microservice/User Management/UserManagement.Service/Interface/IUserService.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UserManagement.Models.Login;
+using UserManagement.Models.Register;
+using UserManagement.Models.UpdatePassword;
+
+namespace UserManagement.Service.Interface
+{
+    public interface IUserService
+    {
+        /// <summary>
+        /// Login
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns></returns>
+        Task<UserDetails> LoginAsync(Login users);
+
+        /// <summary>
+        /// Update Password
+        /// </summary>
+        /// <param name="updatePassword"></param>
+        /// <returns></returns>
+        Task<UpdatePasswordResponse> UpdatePasswordAync(UpdatePassword updatePassword);
+
+        /// <summary>
+        /// Register User
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// /<param name="userDetail"></param>
+        /// <returns></returns>
+        Task<RegisterResponse> Register(string email, string password, Users userDetail);
+
+    }
+}
